c3c5aa924592cb98f60ee73e9020be6fd00a2583
Fuat POYRAZ <fuat@fuatpoyraz.com>
2020-06-01 10:34:41 +0300

    conflict

--- index.js
+++ index.js
@@ -36,37 +36,4 @@ function patch(context, key, value) {
   }
 
   return context[key];
-<<<<<<< HEAD
-=======
-=======
-'use strict'
-
-var toString = require('mdast-util-to-string')
-var visit = require('unist-util-visit')
-var slugify = require('slugify')
-
-module.exports = slug
-
-function slug() {
-  return transformer
-}
-
-// Patch slugs on heading nodes.
-function transformer(ast) {
-  slugs.reset()
-
-  visit(ast, 'heading', visitor)
-
-  function visitor(node) {
-    var data = node.data || (node.data = {})
-    var props = data.hProperties || (data.hProperties = {})
-    var id = props.id
-
-    id = id ? slugify(id, true) : slugify(toString(node))
-
-    data.id = id
-    props.id = id
-  }
->>>>>>> 9296d9272c26063c5fc673b43cced197a903156a
->>>>>>> 5c5364b9fee454057726c9259d6f7191ef7f1b2d
 }
